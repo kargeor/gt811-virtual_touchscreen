f01e9b69e3ad3d4c745db02184a3f46744c19a5d
Georgios Karnas <kargeor@gmail.com>
2020-01-20 12:10:22 -0800

    first

---
+++ Makefile
@@ -0,0 +1,3 @@
+gt811-virtual_touchscreen:
+	g++ main.cpp -o gt811-virtual_touchscreen
+
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPiI2C.h>
+
+int main() {
+  return 0;
+}
+
